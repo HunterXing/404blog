b2115ebd271fc0b2bb956add729f256cce39115c
xingheng <15555306200@163.com>
2019-05-21 11:33:21 +0800

    修改文件夹名

---
+++ fontend/src/views/blog/article/Article.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="article">
+    文章
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Article',
+  methods: {
+    getApi () {
+      axios.get('http://212.64.25.152:5000/test')
+        .then(function (res) {
+          console.log(res)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  },
+  mounted () {
+    this.getApi()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
