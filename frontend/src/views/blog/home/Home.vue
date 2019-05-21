df4261af2e5707f2ed165e5db41d4bed74054483
xingheng <15555306200@163.com>
2019-05-21 15:31:11 +0800

    文章list的样式设计

--- frontend/src/views/blog/home/Home.vue
+++ frontend/src/views/blog/home/Home.vue
@@ -1,103 +1,26 @@
 <template>
   <div class="home">
     <div class="content">
-       <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span class="article-title">CSS实现两个球相交的粘粘效果</span>
-          <el-button style="float: right; padding: 3px 0" type="text"><i class="el-icon-circle-close"></i></el-button>
-        </div>
-        <div class="article-simple-content">
-          <div class="text item">
-          这是一个纯粹利用CSS所做出来的效果，这个效果说穿了就是一个图像处理的原理，做法跟Photoshop里头的几乎一模一样，只是一个用图层和色版来制作，一个则是用CSS（把div当成图层思考就好了）。
-          </div>
-          <!-- 文章图片简略图 -->
-          <div class="poster-box"
-              :style="{backgroundImage: 'url(' + backPic + ')'}"
-              >
-          </div>
-        </div>
-
-        <el-tag
-          class="tag"
-          v-for="tag in tags"
-          :key="tag.name"
-          closable
-          :type="tag.type">
-          {{tag.name}}
-        </el-tag>
-      </el-card>
+      <article-card></article-card>
     </div>
-
   </div>
 </template>
 
 <script>
+import ArticleCard from './components/ArticleCard'
 export default {
   name: 'Home',
+  components: {
+    ArticleCard
+  },
   data () {
     return {
-      tags: [
-        { name: '标签一', type: '' },
-        { name: '标签二', type: 'success' },
-        { name: '标签三', type: 'info' },
-        { name: '标签四', type: 'warning' },
-        { name: '标签五', type: 'danger' }
-      ],
-      backPic: 'http://performancesolutionsbymilliken.com/wp-content/uploads/2017/09/training-blog-header.jpg'
+
     }
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-  .home
-    .content
-      min-height 1000px
-      text-align center
-      // display flex
+<style lang="scss" scoped>
 
-      .item {
-        margin-bottom: 18px;
-      }
-      .clearfix:before,
-      .clearfix:after {
-        display: table;
-        content: "";
-      }
-      .clearfix:after {
-        clear: both
-      }
-      .box-card {
-        margin-top 20px
-        width: 1000px;
-        text-align left
-        display inline-block
-        .article-simple-content {
-          display flex
-          .text {
-            width 400px
-            height 120px
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: 3;
-            overflow: hidden
-            font-size: 14px;
-            line-height: 30px;
-            padding-right 200px
-          }
-          .poster-box {
-            margin-right 200px
-            // flex 4
-            width 150px
-            height 150px
-            background-size 100% 100%
-          }
-        }
-        .tag {
-          margin-right 6px
-        }
-        .article-title {
-          font-weight: 700
-        }
-      }
 </style>
---
+++ frontend/src/views/blog/home/components/ArticleCard.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="aiticle-card">
+    <div class="content">
+      <el-card class="box-card"  shadow="hover">
+        <!-- 左边 -->
+        <div class="left-con">
+          <div class="pic"
+            :style="{backgroundImage: 'url(' + (articlePic[0]) + ')'}"
+          ></div>
+          <p>
+            <span class="p-create-time">创建时间：</span> <span>2019-5-21</span>
+          </p>
+          <span class="p-read-number">有1290人阅读过该文章</span>
+        </div>
+        <!-- 右边 -->
+        <div class="right-con">
+          <div class="article-titie">
+            <span>No1.</span>
+            制作一个简单的菜单动画效果
+          </div>
+          <div class="article-author">
+            <i class="el-icon-s-custom"></i> xing |
+            <i class="el-icon-notebook-2"></i> web前端工程师
+          </div>
+          <div class="article-content">
+            {{articleBrief[0]}}
+          </div>
+          <!-- 标签 -->
+          <div class="tags-con">
+            <el-tag>标签一</el-tag>
+            <el-tag type="success">标签二</el-tag>
+            <el-tag type="info">标签三</el-tag>
+            <el-tag type="warning">标签四</el-tag>
+            <el-tag type="danger">标签五</el-tag>
+          </div>
+        </div>
+
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AiticleCard',
+  data () {
+    return {
+      // 文章图片
+      articlePic: [
+        'http://gss0.baidu.com/9rkZbzqaKgQUohGko9WTAnF6hhy/mms-res/fed/ife/ife_tutor/课程配图.3fb31f3e5af7e8d7.jpg'
+      ],
+      // 文章简述文字
+      articleBrief: [
+        '关于此课程 此课程按照简单到难的顺序一共有八个任务，由浅入深的带大家了解 Web 动效落地方法 。完成这八道题，你会掌握如下技能： 熟练掌握 CSS transition 、transform 、animation 的用法 ; 怎么从一份动效标注 去 100% 还原 CSS 动画 ; 学会使用常用的 前端动画开源库 。 课程适用人群 你需要具备一定 HTML、CSS 开发基础； 你对 动效设计概念 有一定的了解，既知道怎么做，也知道为什么要这么做； 你需要具备熟练使用 git|github 的能力。 作者有话说 在后续的一段时间，我会'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .aiticle-card {
+    .content {
+      margin: 0 20px;
+      text-align: center;
+      .box-card {
+        display: inline-block;
+        margin-top: 50px;
+        width: 1000px;
+        height: 345px;
+        .left-con {
+          height: 100%;
+          width: 40%;
+          text-align: left;
+          float: left;
+          .pic {
+            height: 214px;
+            width: 379px;
+            background-repeat: no-repeat;
+            background-size: cover;
+          }
+        }
+        .right-con {
+          text-align: left;
+          float: right;
+          height: 100%;
+          width: 55%;
+          .article-titie {
+            font-size: 20px;
+            color: #262626;
+          }
+          .article-author {
+            margin-top: 10px;
+            color: #38ba72;
+          }
+          .article-content {
+            margin-top: 20px;
+            font-size: 15px;
+            display: -webkit-box;
+            -webkit-line-clamp: 5;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+            line-height: 1.8;
+          }
+          .tags-con {
+            margin-top: 50px;
+          }
+        }
+
+     }
+   }
+ }
+</style>
