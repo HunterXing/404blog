f6fa1f3422a512c857e1881fad621518f4191062
xingheng <15555306200@163.com>
2019-06-29 20:58:11 +0800

    添加标签前端

--- frontend/src/views/blog/write/Write.vue
+++ frontend/src/views/blog/write/Write.vue
@@ -2,26 +2,46 @@
   <div class="write">
     <my-header @doSubmit="doSubmit"></my-header>
     <div class="write-con">
-      <el-input placeholder="请输入文章标题" v-model="form.articleTitle" class="input-group input-with-select">
+      <el-input
+        placeholder="请输入文章标题"
+        v-model="form.articleTitle"
+        class="input-group input-with-select"
+      >
         <el-select v-model="form.articleType" slot="prepend" placeholder="请选择">
           <el-option label="原创" value="0"></el-option>
           <el-option label="转载" value="1"></el-option>
         </el-select>
       </el-input>
-      <div class="reprint-link-con"  v-show="form.articleType === '1'">
+      <div class="reprint-link-con" v-show="form.articleType === '1'">
         <el-input placeholder="请输入原文链接"></el-input>
+      </div>
+
+      <div class="tags-con">
+        <el-tag
+          :key="tag"
+          v-for="tag in dynamicTags"
+          closable
+          :disable-transitions="false"
+          @close="handleClose(tag)"
+        >{{tag}}</el-tag>
+        <el-input
+          class="input-new-tag"
+          v-if="inputVisible"
+          v-model="inputValue"
+          ref="saveTagInput"
+          size="small"
+          @keyup.enter.native="handleInputConfirm"
+          @blur="handleInputConfirm"
+        ></el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
       </div>
       <!-- <mavon-editor
         class="editor"
         style="height: 100%"
         ref="editor"
         @imgAdd="$imgAdd"
-      ></mavon-editor> -->
-      <mavon-editor
-        class="editor"
-        style="height: 100%"
-        ref="editor"
-      ></mavon-editor>
+      ></mavon-editor>-->
+      <mavon-editor class="editor" style="height: 100%" ref="editor"></mavon-editor>
     </div>
   </div>
 </template>
@@ -42,7 +62,10 @@ export default {
         articleTitle: "",
         articleType: "0",
         articleValue: ""
-      }
+      },
+      dynamicTags: ["标签一", "标签二", "标签三"],
+      inputVisible: false,
+      inputValue: ""
     };
   },
   methods: {
@@ -79,6 +102,27 @@ export default {
         // $vm.$img2Url(pos, url);
         this.$refs.editor.$img2Url(pos, url);
       });
+    },
+
+    // 标签页部分的事件
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+
+    handleInputConfirm() {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        this.dynamicTags.push(inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = "";
     }
   }
 };
@@ -96,17 +140,42 @@ export default {
       margin-top: 50px;
     }
 
-    .input-group, .el-input {
-      border: #ccc 2px solid;
-      border-radius: 5px;
+    .reprint-link-con {
+      .input-group, .el-input {
+        border: #ccc 2px solid;
+        border-radius: 5px;
+      }
     }
 
     >>> .el-input--suffix {
       width: 100px;
     }
+
     .reprint-link-con {
       margin-top: 20px;
     }
+  }
+
+  .tags-con {
+    margin-top: 20px;
+  }
+
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
   }
 }
 </style>
